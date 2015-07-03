6ea288d78c62fe07b48e1913aea591740351f1fa
Le Bras <leo.lebrasf@gmail.com>
2015-07-03 14:24:54 +0200

    Fix profile form

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -9,7 +9,6 @@
 	<form method="POST" action="{{ URL::route('dashboard.account.index') }}">
         @if(Session::has('response'))
             <div class="alerts">
-                @if(Session::get('response') == 'error'))
                 @if(Session::get('response') == 'error')
                 <div class="alert -error">
                     Une erreur est survenue ! Veuillez vérifier que
@@ -34,9 +33,6 @@
                 @endif
             </div>
             <div>
-                <label for="compagny">Entreprise :</label>
-                <input @if(Session::has('compagny')) class="-error" @endif type="text" name="compagny" id="compagny" @if(isset($user) && $user != '') value="{{ $user->compagny }}" @endif value="{{ old('compagny') }}">
-                @if(Session::has('compagny'))
                 <label for="username">Utilsateur :</label>
                 <input @if(Session::has('username')) class="-error" @endif type="text" name="username" id="username" @if(isset($user) && $user != '') value="{{ $user->name }}" @endif value="{{ old('username') }}">
                 @if(Session::has('username'))
@@ -49,6 +45,12 @@
                 <label for="username">Utilsateur :</label>
                 <input @if(Session::has('username')) class="-error" @endif type="text" name="username" id="username" @if(isset($user) && $user != '') value="{{ $user->name }}" @endif value="{{ old('username') }}">
                 @if(Session::has('username'))
+                    <div class="input-alert">
+                        Champ non renseigné.
+                    </div>
+                @endif
+            </div>
+            <div>
                 <label for="website">Website :</label>
                 <input @if(Session::has('website')) class="-error" @endif type="text" name="website" id="website" @if(isset($user) && $user != '') value="{{ $user->website }}" @endif value="{{ old('website') }}">
                 @if(Session::has('website'))
@@ -87,6 +89,15 @@
         	<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
         </fieldset>
         <fieldset>
+            <div>
+                <label for="compagny">Entreprise :</label>
+                <input @if(Session::has('compagny')) class="-error" @endif type="text" name="compagny" id="compagny" @if(isset($user) && $user != '') value="{{ $user->compagny }}" @endif value="{{ old('compagny') }}">
+                @if(Session::has('compagny'))
+                    <div class="input-alert">
+                        Champ non renseigné.
+                    </div>
+                @endif
+            </div>
             <div>
                 <label for="compagny">Entreprise :</label>
                 <input @if(Session::has('compagny')) class="-error" @endif type="text" name="compagny" id="compagny" @if(isset($user) && $user != '') value="{{ $user->compagny }}" @endif value="{{ old('compagny') }}">
