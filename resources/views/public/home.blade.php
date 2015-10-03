aa0162c8fc39bcf35a3d9f6d181f8e8b50aad783
Le Bras <leo.lebrasf@gmail.com>
2015-10-03 16:12:22 +0200

    Add Intercom module

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -257,9 +257,7 @@
 			</footer>
 		</div>
 
-
-        <!-- SUIVI GOOGLE -->
-            <script>
+        <script>
               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -267,11 +265,14 @@
 
               ga('create', 'UA-67066384-1', 'auto');
               ga('send', 'pageview');
-            </script>
-         <!-- // SUIVI GOOGLE -->
-
-         <script src="{{ asset('js/public.min.js') }}"></script>
-
+        </script>
+		<script>
+			window.intercomSettings = {
+				app_id: "ii51ti8x"
+			};
+		</script>
+		<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ii51ti8x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+        <script src="{{ asset('js/public.min.js') }}"></script>
         <script>
             $(document).ready(function(){
                 $('.join').submit(function(e){
