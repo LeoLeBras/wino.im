a3e24b02fb19b1dbef8fbfca7e2b27b8ab03b4c5
Le Bras <leo.lebrasf@gmail.com>
2015-10-06 16:58:12 +0200

    New livechat tool

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -44,12 +44,6 @@
                 color: black;
             }
         </style>
-		<script>
-		  window.intercomSettings = {
-		    app_id: "ii51ti8x"
-		  };
-		</script>
-		<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ii51ti8x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
     </head>
     <body>
     	<div class="loader">
@@ -263,7 +257,7 @@
 				</div>
 			</footer>
 		</div>
-
+		<script type="text/javascript" src="http://livechat.wino.im/php/app.php?widget-init.js"></script>
         <script>
               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
