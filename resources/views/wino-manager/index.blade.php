46545cd6588ab3758f78a0585cba7bdb108ac9ab
Le Bras <leo.lebrasf@gmail.com>
2015-10-08 18:19:47 +0200

    Update wino-manager v0.9.1

--- resources/views/wino-manager/index.blade.php
+++ resources/views/wino-manager/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Wino Manager</title>
-    <link rel="stylesheet" href="{{ asset('/css/wino-manager__9EAzG.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/wino-manager__7E8A2.css') }}">
 </head>
 <body>
     <div class="WinoManager__9EAzG"></div>
